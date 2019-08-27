cb29ce5877ae18828da80fe555911013b4a0106c
Leon <eulertech@users.noreply.github.com>
2019-08-27 15:37:20 -0400

    add api

---
+++ 1000face_util.py
@@ -0,0 +1,91 @@
+import sys
+import os
+import os.path
+import psycopg2
+import json
+
+def get_connection ():
+    try:
+        db_conn = psycopg2.connect(host="thsndfaces.c9dfyqjobtqf.us-east-1.rds.amazonaws.com",
+                                   port=5432,
+                                   database="thsndfaces",
+                                   user="postgres",
+                                   password="1000faces")
+    except:
+        print "Connection Error."
+        sys.exit(1)
+    return db_conn
+
+
+def get_employee ( emp_id ):
+    db_conn = get_connection()
+    cursor = db_conn.cursor()
+    emp_id_str = [str(i) for i in emp_id]
+    sqlString = r"select * from public.employee where id in ({}) ".format(','.join(emp_id_str))
+    print(sqlString)
+    cursor.execute(sqlString)
+    print(sqlString)
+    rows = cursor.fetchall()
+
+    columns = ('firstname', 'degree', 'pastprojectsid', 'skills', 'lastname', 'managerid', 'profilepicturename', 'yearsincompany',
+               'hobbies', 'id','hub')
+    data = []
+    for row in rows:
+        data.append(dict(zip(columns, row)))
+    return json.dumps(data)
+
+def get_project ( pid ):
+    db_conn = get_connection()
+    cursor = db_conn.cursor()
+    p_id_str = [str(i) for i in pid]
+    str1 = "select * from public.projects where pid in ({})".format(','.join(p_id_str))
+    print str1
+    cursor.execute (str1)
+    rows = cursor.fetchall()
+
+    columns = ('pid','projectname','projectteam','projectowner', 'projectdescription', 'projectcategory')
+    data = []
+    for row in rows:
+        data.append(dict(zip(columns, row)))
+    return json.dumps(data)
+
+def get_node (eid):
+    db_conn = get_connection()
+    cursor = db_conn.cursor()
+    emp_id_str = [str(i) for i in eid]
+    sqlString = r"select 'hub' as type, 'Tech' as name, -1 id, 'images/hub/tech.png' as url union " \
+                r"select 'hub' as type, 'Business' as name, -2 id, 'images/hub/business.png' as url union " \
+                r"select 'hub' as type, 'QA' as name, -3 id, 'images/hub/qa.png' as url union " \
+                r"select 'hub' as type, 'ML' as name, -4 id, 'images/hub/ml.png' as url union " \
+                r"select 'person' as type, firstname as name,id, '/imgs/' ||  profilepicturename || '.jfif' as url from public.employee where id in ({}) ".format(','.join(emp_id_str))
+
+    cursor.execute(sqlString)
+
+    rows = cursor.fetchall()
+
+    columns = (
+    'type', 'name', 'id', 'url')
+    data = []
+    for row in rows:
+        data.append(dict(zip(columns, row)))
+
+    columns = ('source', 'target')
+    sqlString = r"select hub as source, firstname as target from public.employee where id in ({}) ".format(
+        ','.join(emp_id_str))
+
+    cursor.execute(sqlString)
+
+    rows = cursor.fetchall()
+    data1 = []
+    for row in rows:
+        data1.append(dict(zip(columns, row)))
+
+    response = {"node": data, "link": data1}
+
+    return json.dumps(response)
+
+if __name__ == "__main__":
+
+    print get_employee([12126, 12127])
+    print get_node([12126, 12127])
+    print get_project([1, 2])
